396ea33b0a77b2116a786c2c6e4d0b161bb670d4
Stefan Nagy <snagy@cs.utah.edu>
2023-05-08 15:13:35 -0600

    Cleanup

--- src/rt_preproc/cli/patch_cmd.py
+++ src/rt_preproc/cli/patch_cmd.py
@@ -2,6 +2,17 @@ from cleo.commands.command import Command
 from cleo.helpers import argument
 from rt_preproc.parser.parser import Parser
 
+# Function to insert "getenv(macro)" stubs.
+
+def add_stubs(tree, macros):
+    for macro in set(macros):
+        tree.insert(0, "/* Variability Stub */\nint rt_%s() {\
+            \n  return getenv(\"%s\") != NULL;\n}\n" % (macro, macro))
+
+    tree.insert(0,"#include <stdlib.h>\n")
+    return tree
+
+# Main class...
 
 class PatchCmd(Command):
     name = "patch"
@@ -14,56 +25,42 @@ class PatchCmd(Command):
     def handle(self):
         file = self.argument("file")
         self.line(f"File: {file}")
+
+        # Read file into f and perform parsing.
         
         with open(file, mode="rb") as f:
-
-            # Read file into f and perform parsing.
-
-            bytes     = f.read()
-            ds        = Parser()
-            tree      = ds.parse(bytes)
-            tree_text = tree.text.decode("utf8")
-            nodes     = ds.query(tree)
-
-            # Set up a "stack" (list) to support parsing.
-
-            seen_blks = []
-
-            # Split tree into lines and perform patching.
-
-            tree_split = tree_text.split('\n')
-            (prefix_line, prefix_offset) = (0,0)
-            (suffix_line, suffix_offset) = (0,0)
-
-            # Go through the tree node-by-node, looking for blocks...
-
-            for (node,name) in nodes:
-                print(seen_blks)
-
-                # If hit "block, grab its bounds and append to our list.
-
-                if name == "block": 
-                    seen_blks.append(node)
-                    (prefix_line, prefix_offset) = node.start_point
-                    (suffix_line, suffix_offset) = node.end_point
-
-                # If hit "macro", rewrite the block as a conditional.
-                
-                if name == "macro" and node.parent == seen_blks[-1]:
-                    macro_text = node.text.decode("utf8")
-
-                    tree_split[prefix_line] = "%sif(%s){" % \
-                        (' '*prefix_offset, macro_text)
-
-                    tree_split[suffix_line] = "%s}" % \
-                        (' '*prefix_offset)
-
-            tree_done = '\n'.join(tree_split)
+            bytes  = f.read()
+            ds     = Parser()
+            tree   = ds.parse(bytes)
+            nodes  = ds.query(tree)
+            tree   = tree.text.decode("utf8").split('\n') 
+            seen   = []
+            macros = []
 
             self.line("---- INPUT C FILE ----")
-            self.line(tree_text)
+            self.line('\n'.join(tree))
+
+            # Visit nodes and process accordingly...
+
+            for (node, name) in nodes:
+                if name == "block": 
+
+                    # Extract macro from first child ('identifier')...
+
+                    macro = node.children[1].text.decode('utf-8')
+                    macros.append(macro)
+
+                    # Convert to conditional based on bounds and macro...
+
+                    (start,end) = (node.start_point, node.end_point)
+                    tree[start[0]] = "%sif(rt_%s()){" % (' '*start[1], macro)
+                    tree[end[0]]   = "%s}" % (' '*start[1])
+
+            tree = add_stubs(tree, macros) # Add helper stubs...
+
+            # Done!
 
             self.line("---- OUTPUT C FILE ----")
-            self.line(tree_done)
+            self.line('\n'.join(tree))
 
 
