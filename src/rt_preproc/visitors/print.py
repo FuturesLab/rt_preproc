c48ab09b61f985d5c698eaa6181630e4c168d74b
Andrew Lee <candrewlee14@gmail.com>
2023-09-10 15:57:39 -0600

    clean up print.py

--- src/rt_preproc/visitors/print.py
+++ src/rt_preproc/visitors/print.py
@@ -1,29 +1,20 @@
 from typing import Any
 import rt_preproc.parser.ast as ast
-from typing import Optional
 from multimethod import multimethod
 from rt_preproc.visitors.base import IVisitor, IVisitorCtx
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 class PrintCtx(IVisitorCtx):
-    def __init__(self, parent: Optional[ast.AstNode] = None) -> None:
-        self.parent = parent
+    pass
 
 
 class PrintVisitor(IVisitor):
-    """Visitor for performing variability Printations on AST nodes."""
-
-    def __init__(self) -> None:
-        self.buf = ""
-        self.seen = []
+    """Visitor for printing an AST."""
 
     @multimethod
     def visit(self, node: ast.AstNode, ctx: PrintCtx) -> Any:
-        if node.children is not None and len(node.children) > 0:
+        if len(node.children) > 0:
             for child in node.children:
-                child.accept(self, PrintCtx(parent=node))
+                child.accept(self, ctx)  # ctx is not used in this visitor
         else:  # leaf node
             print(node.text, end="")
